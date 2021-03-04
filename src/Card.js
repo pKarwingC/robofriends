5ac156a3bc096c8552c06fdcc452c84a11e87e86
peter <peter@grenadine.co>
2021-03-04 06:30:23 -0500

    created a backbone for the robofriends project

---
+++ src/Card.js
@@ -0,0 +1,15 @@
+import React from 'react';
+
+const Card = ({})=> {
+    return (
+        <div>
+            <img alt="robofriend-img" src="https://robohash.org/test" />
+            <div>
+                <h2>Hi</h2>
+                <p>Hello</p>
+            </div>
+        </div>
+    );
+}
+
+export default Card
