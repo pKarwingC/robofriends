8d8b8ea7744d927977e8f6ebc3c6b4c9bb51fdf8
peter <peter@grenadine.co>
2021-03-04 07:06:40 -0500

    added robot cards

--- src/Card.js
+++ src/Card.js
@@ -1,12 +1,12 @@
 import React from 'react';
 
-const Card = ({})=> {
+const Card = ({name, id, email})=> {
     return (
-        <div>
-            <img alt="robofriend-img" src="https://robohash.org/test" />
-            <div>
-                <h2>Hi</h2>
-                <p>Hello</p>
+        <div className="bg-light-green dib br3 pa3 ma2 grow bw2 shadow-5">
+            <img alt="robofriend-img" src={`https://robohash.org/${id}`} />
+            <div >
+                <h2>{name}</h2>
+                <p>{email}</p>
             </div>
         </div>
     );
--- src/index.js
+++ src/index.js
@@ -3,9 +3,10 @@ import ReactDOM from 'react-dom';
 import './index.css';
 import Card from './Card';
 import reportWebVitals from './reportWebVitals';
+import { robots } from "./robots";
 import 'tachyons';
 
-ReactDOM.render(<Card />, document.getElementById('root'));
+ReactDOM.render(<Card id={robots[0].id} name={robots[0].name} email={robots[0].email} />, document.getElementById('root'));
 
 // If you want to start measuring performance in your app, pass a function
 // to log results (for example: reportWebVitals(console.log))
